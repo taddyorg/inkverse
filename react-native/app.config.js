765ec4974a8e215037e3e8759a19a3f9f15ee4cf
Daniel Mathews <dannyl.mathews@gmail.com>
2025-11-27 10:42:09 -0800

    deploy new ios and android apps

--- react-native/app.config.js
+++ react-native/app.config.js
@@ -11,7 +11,7 @@ export default {
     jsEngine: "hermes",
     ios: {
       bundleIdentifier: "art.bamcomics.taddy",
-      buildNumber: "2",
+      buildNumber: "3",
       supportsTablet: false,
       infoPlist: {
         LSApplicationQueriesSchemes: [
@@ -29,7 +29,7 @@ export default {
     },
     android: {
       package: "com.bamtoons",
-      versionCode: 97,
+      versionCode: 98,
       minSdkVersion: 35,
       adaptiveIcon: {
         foregroundImage: "./assets/icons/adaptive-icon.png",
